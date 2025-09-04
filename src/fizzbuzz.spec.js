a41636039f8e849e4b3f4eb3d1f1c9a4d6c99ee6
Lucas-RodriguezAncieta <lucassantiago.lsra@gmail.com>
2025-09-04 17:18:09 -0400

    feat: prueba agregada caso retorno de FizzBuzz cuando es multiplo de 3 y 5

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -1,16 +1,24 @@
-const fizzBuzz = require('./fizzbuzz');
+
+const fizzBuzz = require('./fizzbuzz'); // Importamos la función que vamos a crear
 
 test('debería retornar 1 para el número 1', () => {
     expect(fizzBuzz(1)).toBe('1');
 });
 
+test('debería retornar 1 para el número 1', () => {
+    expect(fizzBuzz(4)).toBe('4');
+});
 
 test('debería retornar Fizz para múltiplos de 3', () => {
     expect(fizzBuzz(3)).toBe('Fizz');
-    expect(fizzBuzz(6)).toBe('Fizz');
 });
 
 test('debería retornar Buzz para múltiplos de 5', () => {
     expect(fizzBuzz(5)).toBe('Buzz');
-    expect(fizzBuzz(10)).toBe('Buzz');
 });
+
+
+test('debería retornar FizzBuzz para múltiplos de 3 y 5', () => {
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
+});
+
